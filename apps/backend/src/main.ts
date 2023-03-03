0c43ad379e50869a5fc567212e7c264b72ac573a
smoothbronx <vlad.uhwatov@yandex.ru>
2023-03-03 19:21:22 +0200

    Change backend application port

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,33 +1,33 @@
-import { TransformInterceptor } from '@/shared/interceptors/transform.interceptor';
-import { INestApplication, ValidationPipe } from '@nestjs/common';
-import { SwaggerOptions } from '@/swagger/swagger.options';
-import { swaggerConfig } from '@/swagger/swagger.config';
-import { SwaggerModule } from '@nestjs/swagger';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { TransformInterceptor } from "@/shared/interceptors/transform.interceptor";
+import { INestApplication, ValidationPipe } from "@nestjs/common";
+import { SwaggerOptions } from "@/swagger/swagger.options";
+import { swaggerConfig } from "@/swagger/swagger.config";
+import { SwaggerModule } from "@nestjs/swagger";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-    const application = await NestFactory.create(AppModule);
-    application.setGlobalPrefix('/api/');
-    enableSwagger(application, 'api/docs/');
+  const application = await NestFactory.create(AppModule);
+  application.setGlobalPrefix("/api/");
+  enableSwagger(application, "api/docs/");
 
-    application.useGlobalInterceptors(new TransformInterceptor());
-    application.useGlobalPipes(
-        new ValidationPipe({
-            transform: true,
-            transformOptions: { enableImplicitConversion: true },
-        }),
-    );
+  application.useGlobalInterceptors(new TransformInterceptor());
+  application.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    })
+  );
 
-    await application.listen(3000);
+  await application.listen(4000);
 }
 
 function enableSwagger(application: INestApplication, path: string) {
-    const document = SwaggerModule.createDocument(application, swaggerConfig);
-    const options = new SwaggerOptions();
-    options.setDarkTheme();
+  const document = SwaggerModule.createDocument(application, swaggerConfig);
+  const options = new SwaggerOptions();
+  options.setDarkTheme();
 
-    SwaggerModule.setup(path, application, document, options);
+  SwaggerModule.setup(path, application, document, options);
 }
 
 bootstrap();
